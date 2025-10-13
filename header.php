027f7e2b44e93602daa33e10a9482b248d6ad95f
Darinel Nieto <darinelnieto@Darinels-MacBook-Pro.local>
2025-10-13 16:13:37 -0500

    Header

--- header.php
+++ header.php
@@ -13,6 +13,13 @@ if ( ! defined( 'ABSPATH' ) ) {
 <html class="no-js" <?php language_attributes(); ?>>
 
 <head>
+  <!-- Google Tag Manager -->
+  <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+  })(window,document,'script','dataLayer','GTM-MZ8G68SJ');</script>
+  <!-- End Google Tag Manager -->
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
   <title><?php bloginfo('name'); ?> - <?php wp_title('|', true, 'right'); ?></title>
@@ -47,6 +54,10 @@ if ( ! defined( 'ABSPATH' ) ) {
 </head>
 
 <body <?php body_class(); ?>>
+  <!-- Google Tag Manager (noscript) -->
+  <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MZ8G68SJ"
+  height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+  <!-- End Google Tag Manager (noscript) -->
 <div id="page"> <!-- +Page container -->
 
   <header id="header-wrapper">
