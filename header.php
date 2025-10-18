2da9bf1b747b264cbdf20873aac2725ad4317f5e
Darinel Nieto <darinelnieto@Darinels-MacBook-Pro.local>
2025-10-18 18:53:02 -0500

    Title

--- header.php
+++ header.php
@@ -23,7 +23,6 @@ if ( ! defined( 'ABSPATH' ) ) {
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
   <title><?php wp_title('|', true, 'right'); ?></title>
-  <meta name="description" content="">
   <meta name="author" content="">
   <meta name="viewport" content="width=device-width">
   <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
